b295baec4aaf0f3af1345f75b84281e98540e045
chislee0708 <chislee0708@gmail.com>
2020-10-05 11:46:43 -0400

    Demo
    Demo

---
+++ Assets/SceneConroller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneConroller : MonoBehaviour
+{
+    private int nextScenceLoad;
+
+    private void Start()
+    {
+        nextScenceLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(nextScenceLoad);
+    }
+}
