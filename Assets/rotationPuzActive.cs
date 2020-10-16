095d7682d847b2af282b0c949504b391370decb4
alyona <tiki2012.ap@gmail.com>
2020-10-15 20:14:34 -0400

    add valve puzzle prototype

---
+++ Assets/rotationPuzActive.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationPuzActive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider colidedObj)
+    {
+        if (colidedObj.gameObject.tag == "rotation_puzzle_tag")
+        {
+            //DoorAnimation.Play();
+            //soundDoor = GetComponent<AudioSource>();
+            //soundDoor.clip = audio;
+
+            //duration = audio.length;
+            //soundDoor.Play();
+
+            //StartCoroutine(WaitForSound());
+            print("col");
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+    }
+    //IEnumerator WaitForSound()
+    //{
+    //    yield return new WaitForSeconds(duration - 1);
+    //    Debug.Log("FinishAudio");
+
+   
+    //    loadLevel.LoadingLevel(1);
+
+   //}
+}
