c7fe6d30920fed816a8ad4fffa1d9f3baa679225
briancc101 <brianchambers876@gmail.com>
2020-10-29 06:56:52 -0400

    Grab script
    Added script to grab puzzle pieces

---
+++ Assets/Scripts/MovePuzzlePiece.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePuzzlePiece : MonoBehaviour
+{   
+
+    public GameObject puzzlePiece;
+    public GameObject playerHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            puzzlePiece.transform.SetParent(playerHand.transform);
+            puzzlePiece.transform.localPosition = new Vector3(0f, -0.4f, 0f);
+        }
+    }
+}
