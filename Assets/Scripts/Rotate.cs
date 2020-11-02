2dad21c47e8083a8c5f4abf9b42882d3c1916ad9
alyona <tiki2012.ap@gmail.com>
2020-11-02 13:49:11 -0500

    add puzzle 3, add assets

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Yellow()
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    public void RotateObj()
+    {
+        transform.Rotate(0, 90, 0);
+    }
+
+    public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
