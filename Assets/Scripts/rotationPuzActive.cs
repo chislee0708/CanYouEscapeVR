97c22847866ba75f05a4335aa549c8426c728053
alyona <tiki2012.ap@gmail.com>
2020-11-05 00:23:48 -0500

    add key and key holder

--- Assets/Scripts/rotationPuzActive.cs
+++ Assets/Scripts/rotationPuzActive.cs
@@ -7,6 +7,7 @@ public class rotationPuzActive : MonoBehaviour
 
     public GameObject[] pipes;
     public Material originalMaterialB;
+    public bool activator = false; //true if piece of puzzle solved
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,7 @@ public class rotationPuzActive : MonoBehaviour
 
     void OnTriggerEnter(Collider colidedObj)
     {
-        if (colidedObj.gameObject.tag == "rotation_puzzle_tag")
+        if (colidedObj.gameObject.tag == "rotation_puzzle_tag") // if puzzle rotated correctly and got to its place
         {
             //DoorAnimation.Play();
             //soundDoor = GetComponent<AudioSource>();
@@ -26,25 +27,24 @@ public class rotationPuzActive : MonoBehaviour
 
             //StartCoroutine(WaitForSound());
             
-            GetComponent<Renderer>().material.color = Color.green;
+            //GetComponent<Renderer>().material.color = Color.green; //change color to green
            
-            pipes = GameObject.FindGameObjectsWithTag("pipe");
+            pipes = GameObject.FindGameObjectsWithTag("pipe"); //pipes are all objects with tag "pipe"
             for (int i = 0; i < pipes.Length; i++)
             {
-                pipes[i].GetComponent<MeshRenderer>().material.color = Color.green;
+                //pipes[i].GetComponent<MeshRenderer>().material.color = Color.green;  //change color to green
+                activator = true; // mark true because piece of puzzle solved
             }
 
-        } else
-        {
-            GetComponent<Renderer>().material.color = Color.blue;
-        }
-
+        } 
     }
+   
     void OnTriggerExit(Collider colidedObj)
     {
         if (colidedObj.gameObject.tag == "rotation_puzzle_tag")
         {
-            GetComponent<Renderer>().material = originalMaterialB;
+            //GetComponent<Renderer>().material = originalMaterialB; // change to original color/material
+            activator = false; // mark false because piece of puzzle solved incorrectly
         }
 
     }
