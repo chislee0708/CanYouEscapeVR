5cc5d847e69def121986ac353613b5aedc14d89a
chislee0708 <chislee0708@gmail.com>
2020-11-09 19:02:17 -0500

    Added GUI in Scene1
    Added description of the game GUI, show and hide menu script, and menu interactions script.

---
+++ Assets/Scripts/ShowHideInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideInfo : MonoBehaviour
+{
+    public GameObject InfoObject;
+    private bool Show = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Showing and hidding object
+    public void ShowAndHideInfo()
+    {
+        if (!Show)
+        {
+            InfoObject.SetActive(true);
+            Show = true;
+        }
+        else
+        {
+            InfoObject.SetActive(false);
+            Show = false;
+        }
+    }
+}
