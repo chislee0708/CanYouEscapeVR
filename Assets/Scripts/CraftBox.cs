a7df5d08572b45140519f6c51a04e083f98325c6
TDLorenz <troydaniello.nyc@gmail.com>
2020-11-23 22:47:20 -0500

    Push 1

--- Assets/Scripts/CraftBox.cs
+++ Assets/Scripts/CraftBox.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
 public class CraftBox : MonoBehaviour
 {
-    Vector3 waypoint;
     public bool isComplete = false;
     // List for ingredient tags
     public List<string> ingTags = new List<string>(); //gameObject.tag is string
@@ -26,67 +26,55 @@ public class CraftBox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    // adding and deletig item func, may just need to use modififed drop from drop and grab to alsoe delete item
-
-
-    // item check func
-    // node master script for item?
-    /*bool ItemChosen()// (item player is holding)
-    {
-        bool itemAdded = OnCollisionsEnter(item);
-        return itemAdded;
-    }
-    */
+    
 
     // Used for adding items to craftbox
     void OnCollisionsEnter(Collision col)
     {
-        if (col.gameObject.tag == "ing1" || col.gameObject.tag == "ing2" || col.gameObject.tag == "ing3" || col.gameObject.tag == "ing4" || col.gameObject.tag == "ing5")
+        //if (col.gameObject.tag == "ing1" || col.gameObject.tag == "ing2" || col.gameObject.tag == "ing3" || col.gameObject.tag == "ing4" || col.gameObject.tag == "ing5")
+        // {
+        Debug.Log("Added " + col.gameObject.tag); // test script
+        ingTags.Add(col.gameObject.tag);
+        string tag = col.gameObject.tag;
+        char c = tag[3];
+        int objectNum = (int)(c - '0'); // makes tag ing num an integer for ings array purposes
+        objectNum -= 1; // allligns ing# with array pos
+        ings[objectNum].SetActive(false); // makes corresponding ingredient objcet invisible?
+
+
+        // Remove obj material (make obj invisible and send it to outside room loc) ?
+        // rend.sharedMaterial = material[0]; //??? remove and make invisible? 
+        // May not be nescessary to move obj ing until rresetting and then setactive true
+        int numOfIngs = ingTags.Count;
+        if (numOfIngs > 2) // 3 ings to make medpack 
         {
-            Debug.Log("Added " + col.gameObject.tag); // test script
-            ingTags.Add(col.gameObject.tag);
-            string tag = col.gameObject.tag;
-            char c = tag[3];
-            int objectNum = (int)(c-'0'); // makes tag ing num an integer for ings array purposes
-            objectNum -= 1; // allligns ing# with array pos
-            ings[objectNum].SetActive(false); // makes corresponding ingredient objcet invisible?
-           
-            waypoint = transform.position; //?
-
-
-            // Remove obj material (make obj invisible and send it to outside room loc) ?
-            // rend.sharedMaterial = material[0]; //??? remove and make invisible? 
-            // May not be nescessary to move obj ing until rresetting and then setactive true
-            int numOfIngs = ingTags.Count; 
-            if (numOfIngs > 2) // 3 ings to make medpack 
+            isComplete = transformCraftbox();
+            if (isComplete == true)
             {
-                isComplete = transformCraftbox();     
-                if (isComplete == true)
-                {
-                    // make medpack object appear
-                    medpack.transform.position = new Vector3(2, 1, 4);
-                    // Destroys craftbox as puzzle is complete
-                    GameObject.Destroy(this.gameObject);
-                }
-                else // puzzle is not solved
-                {
-                    Reset();
-                }
+                // make medpack object appear
+                medpack.transform.position = new Vector3(2, 1, 4);
+                // Destroys craftbox as puzzle is complete
+                GameObject.Destroy(this.gameObject);
             }
-        }            
+            else // puzzle is not solved
+            {
+                Reset();
+            }
+        }
+        // }
     }
 
-    bool transformCraftbox() 
+    bool transformCraftbox()
     {
         string combination = "";
-        foreach(var ing in ingTags)
+        foreach (var ing in ingTags)
         {
             combination += ing;
         }
-        if(combination == "ing1ing2ing3")
+        if (combination == "ing1ing2ing3")
         {
             return true;
         }
@@ -96,12 +84,7 @@ public class CraftBox : MonoBehaviour
             return false;
         }
     }
-    /*
-    bool completePuzzle(Collision col)
-    {
-        // use list combo to determine if correct else call reset 
-    }
-    */
+  
 
     // reset items and start agin func
     void Reset()
@@ -113,7 +96,7 @@ public class CraftBox : MonoBehaviour
     void resetIngredients()
     {
         ingTags.Clear(); // clears list of ings
-        for(int i = 0; i < 5; i++) // makes ingredients reappear
+        for (int i = 0; i < 5; i++) // makes ingredients reappear
         {
             ings[i].SetActive(true);
         }
@@ -122,30 +105,106 @@ public class CraftBox : MonoBehaviour
         ings[1].transform.position = new Vector3(2f, 1f, 4.829f);
         ings[2].transform.position = new Vector3(2f, 0.125f, 0.125f);
         ings[3].transform.position = new Vector3(0f, 0.125f, 0.125f);
-        ings[4].transform.position = new Vector3(5.75f, 0.125f, 4f);   
-    } 
+        ings[4].transform.position = new Vector3(5.75f, 0.125f, 4f);
+    }
 }
-// way to possibly destroy object
-/*
-void Update()
+*/
+
+public class CraftBox : MonoBehaviour
+{
+    public bool isComplete = false;
+    // List for ingredient tags
+    public List<string> ingTags = new List<string>(); //gameObject.tag is string
+    public GameObject[] ings = new GameObject[5]; // arr for ingredients
+    public GameObject medpack; // for calling medpacj object at end
+    //Random random = new Random();
+    //public int i = random.Next(0, GameObject.length);
+
+    // Start is called before the first frame update
+    void Start()
     {
-        float rightClick = player_Input.Player.mouseClick.ReadValue<float>();
 
-        Ray point = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
 
-        RaycastHit hit;
+    }
 
-        if (Physics.Raycast(point, out hit))
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    // Used for adding items to craftbox
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("ing1") || col.gameObject.CompareTag("ing2") || col.gameObject.CompareTag("ing3") || col.gameObject.CompareTag("ing4") || col.gameObject.CompareTag("ing5"))
+        //if (col.gameObject.tag == "ing1" || col.gameObject.tag == "ing2" || col.gameObject.tag == "ing3" || col.gameObject.tag == "ing4" || col.gameObject.tag == "ing5")
         {
-            if(hit.transform.tag == "Fence")
+            Debug.Log("Added " + col.gameObject.tag); // test script
+            ingTags.Add(col.gameObject.tag);
+            string tag = col.gameObject.tag;
+            char c = tag[3];
+            int objectNum = (int)(c - '0'); // makes tag ing num an integer for ings array purposes
+            objectNum -= 1; // allligns ing# with array pos
+            ings[objectNum].SetActive(false); // makes corresponding ingredient objcet invisible?
+
+            // Remove obj material (make obj invisible and send it to outside room loc) ?
+            int numOfIngs = ingTags.Count;
+            if (numOfIngs > 2) // 3 ings to make medpack 
             {
-                Destroy(hit.transform.gameObject);
+                isComplete = transformCraftbox();
+                if (isComplete == true)
+                {
+                    // make medpack object appear
+                    medpack.transform.position = new Vector3(2f, 2f, 4.5f);
+                    // Destroys craftbox as puzzle is complete
+                    GameObject.Destroy(this.gameObject);
+                }
+                else // puzzle is not solved
+                {
+                    Reset();
+                }
             }
         }
+    }
 
-        if(rightClick > 0)
+    bool transformCraftbox()
+    {
+        string combination = "";
+        foreach (var ing in ingTags)
         {
-            Debug.Log("Right button has been clicked");
+            combination += ing;
+        }
+        if (combination == "ing1ing2ing3" || combination == "ing1ing3ing2" || combination == "ing2ing1ing3" || combination == "ing2ing3ing1" || combination == "ing3ing1ing2" || combination == "ing3ing2ing1")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
-*/
+
+    // reset items and start agin func
+    void Reset()
+    {
+        resetIngredients();
+        // reset craftbox 
+    }
+
+    void resetIngredients()
+    {
+        ingTags.Clear(); // clears list of ings
+        for (int i = 0; i < 5; i++) // makes ingredients reappear
+        {
+            ings[i].SetActive(true);
+        }
+        // ings assigned to old locations 
+        ings[0].transform.position = new Vector3(4.3f, 2.125f, 14f);
+        ings[1].transform.position = new Vector3(6.5f, 2.125f, 14f);
+        ings[2].transform.position = new Vector3(4.8f, 2.125f, 14f);
+        ings[3].transform.position = new Vector3(3.4f, 2.125f, 14f);
+        ings[4].transform.position = new Vector3(5.75f, 2.125f, 14f);
+
+        ingTags.Clear(); // clears list of ing tags
+    }
+}
+
--- Assets/Scripts/MedPack.cs
+++ Assets/Scripts/MedPack.cs
@@ -7,12 +7,86 @@ public class MedPack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    /*
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class MultipleNodeMasters : MonoBehaviour
+    {
+        public Rigidbody[] Objects = new Rigidbody[5]; // ings 
+        public Transform Holder;
+        public GameObject TheNodes;
+        Random random = new Random();
+        public int i = random.Next(0, Objects.length);
+        public float timeLeft = 20.0f; // for now uses a timer to switch beetenn objects taht player can hold randomly
+        public Rigidbody GazeObject = Objects[Random.Range(0, 4)];
+        // will try to implement line of sight perhpas if workaround works
+        // public Rigidbody GazeObject = Objects[i]; 
+
+        void Update()
+        {
+            CountDown();
+        }
+
+        public void InHands()
+        {
+            GazeObject.transform.parent = Holder.transform;
+            GazeObject.transform.localPosition = Holder.transform.localPosition;
+
+            TheNodes.SetActive(true);
+
+        }
+
+        public void OnNodes()
+        {
+            GazeObject.transform.parent = null;
+            GazeObject.transform.localPosition = Holder.transform.localPosition;
+
+            TheNodes.SetActive(false);
+        }
+
+        public void changeObject()
+        {
+            GazeObject = Objects[Random.Range(0, 4)];
+            //i = random.Next(0, Objects.length)
+        }
+
+        public void CountDown()
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                changeObject();
+                timeLeft = 20.0f;
+            }
+        }
+
+
+
+            public void ObjectChosen()
+            {
+                Vector3 directionToTarget = Holder.transform.position - GazeObjects[i].position;
+                float angle = Vector3.Angle(transform.forward, directionToTarget);
+                float distance = directionToTarget.magnitude;
+
+                if (Mathf.Abs(angle) < 90  distance < 10)
+                {
+                     Debug.Log("target is in front of me");
+                }
+            }
+
+    }
+    */
+
+
 }
+
