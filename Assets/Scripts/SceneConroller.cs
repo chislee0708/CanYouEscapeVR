6922d1a4f10ec90d925c6377206ee5ec1feb17dc
chislee0708 <chislee0708@gmail.com>
2020-11-23 23:05:52 -0500

    Added portal to scene 3

--- Assets/Scripts/SceneConroller.cs
+++ Assets/Scripts/SceneConroller.cs
@@ -5,15 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class SceneConroller : MonoBehaviour
 {
+    public string DoorColor;
     private int nextScenceLoad;
 
     private void Start()
     {
-        nextScenceLoad = SceneManager.GetActiveScene().buildIndex + 1;
+        //nextScenceLoad = SceneManager.GetActiveScene().buildIndex + 1;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(nextScenceLoad);
+        if (DoorColor == "Green")
+        {
+            SceneManager.LoadScene("New-Scene3-Alyona");
+        }
+
+        //Blue
+        //if (DoorColor == "Blue")
+        //{
+        //    SceneManager.LoadScene("");
+        //}
+
+        //Red
+        //if (DoorColor == "Red")
+        //{
+        //    SceneManager.LoadScene("");
+        //}
+
+        //SceneManager.LoadScene(nextScenceLoad);
     }
 }
